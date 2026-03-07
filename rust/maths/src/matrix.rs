3df4f7b1a8c76790c11f18827c1222edac29c1d0
Joe Sharp <ratracejoe@hotmail.com>
2026-03-07 14:29:00 +0000

    triangular form detection

--- rust/maths/src/matrix.rs
+++ rust/maths/src/matrix.rs
@@ -1,6 +1,8 @@
 use crate::approx_eq::ApproxEq;
 #[allow(unused_imports)]
 use crate::matrix;
+use std::fmt;
+use std::fmt::Display;
 
 #[derive(PartialEq, Debug)]
 pub struct Matrix {
@@ -15,6 +17,32 @@ pub enum MatrixError {
     IncompatibleDimensions,
     SquareMatrixRequired,
     InvalidIndex(usize, usize),
+}
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "rows: {}, columns: {}", self.rows, self.columns)?;
+        for r in 0..self.rows {
+            write!(f, "[")?;
+
+            for c in 0..self.columns {
+                match self.get(r, c) {
+                    Ok(v) => write!(f, "{}", v),
+                    _ => return Err(fmt::Error),
+                }?;
+                if c + 1 != self.columns {
+                    write!(f, " ")?
+                }
+            }
+
+            write!(f, "]")?;
+            if r + 1 != self.rows {
+                writeln!(f)?;
+            }
+        }
+
+        Ok(())
+    }
 }
 
 impl ApproxEq for Matrix {
@@ -178,6 +206,39 @@ impl Matrix {
         }
 
         Ok(result)
+    }
+
+    pub fn is_square(&self) -> bool {
+        self.rows == self.columns
+    }
+
+    pub fn is_upper_triangular(&self) -> bool {
+        for row in 0..self.rows {
+            let limit = if row > self.columns {
+                self.columns
+            } else {
+                row
+            };
+            for col in 0..limit {
+                if self.get(row, col) != Ok(0.0) {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+
+    pub fn is_lower_triangular(&self) -> bool {
+        for row in 0..self.rows {
+            for col in (row + 1)..self.columns {
+                if self.get(row, col) != Ok(0.0) {
+                    return false;
+                }
+            }
+        }
+
+        true
     }
 }
 
@@ -395,5 +456,197 @@ mod tests {
         let result = m1.determinant().expect_err("Only works for square");
 
         assert_eq!(result, MatrixError::SquareMatrixRequired);
+    }
+
+    struct FormCase {
+        m: Matrix,
+        is_upper_triangular: bool,
+        is_lower_triangular: bool,
+    }
+
+    fn form_cases() -> Vec<FormCase> {
+        vec![
+            FormCase {
+                m: matrix!(
+                    rows: 2,
+                    cols: 2,
+                    1, 1;
+                    0, 1
+                ),
+                is_upper_triangular: true,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 3,
+                    1, 2, 3,
+                    0, 4, 5,
+                    0, 0, 6
+                ),
+                is_upper_triangular: true,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 3,
+                    1, 0, 0,
+                    2, 3, 0,
+                    4, 5, 6
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: true,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 3,
+                    1, 0, 1,
+                    2, 3, 0,
+                    4, 5, 6
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: Matrix::identity(3),
+                is_upper_triangular: true,
+                is_lower_triangular: true,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 3,
+                    1, 2, 3;
+                    4, 5, 6;
+                    7, 8, 9
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 5,
+                    1, 2, 3, 4, 5;
+                    0, 6, 7, 8, 9;
+                    0, 0, 1, 2, 3
+                ),
+                is_upper_triangular: true,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 5,
+                    cols: 3,
+                    1, 2, 3;
+                    0, 4, 5;
+                    0, 0, 6;
+                    0, 0, 0;
+                    0, 0, 0
+                ),
+                is_upper_triangular: true,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 5,
+                    1, 2, 3, 4, 5;
+                    0, 6, 7, 8, 9;
+                    1, 0, 1, 2, 3
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 5,
+                    cols: 3,
+                    1, 2, 3;
+                    0, 4, 5;
+                    0, 0, 6;
+                    0, 0, 0;
+                    0, 0, 1
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 5,
+                    1, 0, 0, 0, 0;
+                    2, 6, 0, 0, 0;
+                    5, 3, 1, 0, 0
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: true,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 5,
+                    cols: 3,
+                    1, 0, 0;
+                    4, 4, 0;
+                    4, 4, 6;
+                    4, 4, 4;
+                    4, 4, 4
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: true,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 3,
+                    cols: 5,
+                    1, 0, 0, 0, 1;
+                    2, 6, 0, 0, 0;
+                    5, 3, 1, 0, 0
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: false,
+            },
+            FormCase {
+                m: matrix!(
+                    rows: 5,
+                    cols: 3,
+                    1, 0, 1;
+                    4, 4, 0;
+                    4, 4, 6;
+                    4, 4, 4;
+                    4, 4, 4
+                ),
+                is_upper_triangular: false,
+                is_lower_triangular: false,
+            },
+        ]
+    }
+
+    #[test]
+    fn is_upper_triangular() {
+        for case in form_cases() {
+            let result = case.m.is_upper_triangular();
+
+            assert_eq!(
+                result, case.is_upper_triangular,
+                "{} should be upper triangular",
+                case.m
+            );
+        }
+    }
+
+    #[test]
+    fn is_lower_triangular() {
+        for case in form_cases() {
+            let result = case.m.is_lower_triangular();
+
+            assert_eq!(
+                result, case.is_lower_triangular,
+                "{} should be lower triangular",
+                case.m
+            );
+        }
     }
 }
